15a343900859b19f6decd12bc4091f9cfeea42da
juninalmeida <junioralmeidati2023@gmail.com>
2026-03-15 17:17:33 -0300

    chore: add ESLint, Prettier and dotenv configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.get("/health", (_req, res) => { // (_)  silencia o aviso de variáveis não declaradas intencionalmente. "não vou usar"
-  res.json({ status: "Orbita API Online!" });
-});
+app.get('/health', (_req, res) => {
+  // (_)  silencia o aviso de variáveis não declaradas intencionalmente. "não vou usar"
+  res.json({ status: 'Orbita API Online!' })
+})
 
-export { app };
+export { app }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
-import { app } from "./app";
+import 'dotenv/config'
+import { app } from './app'
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, () => {
-  console.log(`Orbita API rodando na porta ${PORT}`);
-});
+  console.log(`Orbita API rodando na porta ${PORT}`)
+})
